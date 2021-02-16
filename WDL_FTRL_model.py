c7d84ae2182c43220145f193571b48a98e9604df
Liu_Yu <yu.liu@banxiniot.com>
2021-02-16 14:13:23 -0500

    modified script for the preprocessed data with HASHING TRICK for all features TOGETHER

--- WDL_FTRL_model.py
+++ WDL_FTRL_model.py
@@ -28,6 +28,7 @@ class WideDeep(nn.Module):
 
     Parameters:
     --------
+    DEEP
     - embeddings_input (tuple): 3-elements tuple with the embeddings "set-up" -
     (col_name, unique_values, embeddings dim)
     - continuous_cols (list) : list with the name of the continuum columns
@@ -35,12 +36,18 @@ class WideDeep(nn.Module):
     their corresponding index in the deep-side input tensor
     - hidden_layers (list) : list with the number of units per hidden layer
     - encoding_dict (dict) : dictionary with the label-encode mapping
-    - n_class (int) : number of classes. Defaults to 1 if logistic or regression
+    
     - dropout (float)
-
-    ====WIDE
+    
+    WIDE:
+    - num_total_wide_features: number of wide features, is used to tell the size of w_array one the 2nd dimension for a single data observation, including interactive features....
+    - num_deep_features: not usefule in this verison
     - D : similar to wide_dim, number of weights (including bias) to use for the "wide" part
-    - interaction: whether to use interaction in the "wide" part
+
+    OTHERS:
+    - n_class (int) : number of classes. Defaults to 1 if logistic or regression
+    ====WIDE
+
     """
     def __init__(self,
                  embeddings_input,
@@ -50,13 +57,11 @@ class WideDeep(nn.Module):
                  batch_norm,
                  dropout,
                  n_class,
-                 num_wide_features,
-                 num_deep_features,
-                 ordered_wide_cols,
-                 D,
-                 interaction = False):
+                 num_total_wide_features,
+                 D):
 
         super(WideDeep, self).__init__()
+        #------deep part
         # self.wide_dim = wide_dim
         self.deep_column_idx = deep_column_idx
         self.embeddings_input = embeddings_input
@@ -66,9 +71,19 @@ class WideDeep(nn.Module):
         self.dropout = dropout
         # self.encoding_dict = encoding_dict
         self.n_class = n_class
-        self.num_wide_features = num_wide_features
-        self.num_deep_features = num_deep_features
-        self.wide_cols = ordered_wide_cols
+        self.num_total_wide_features = num_total_wide_features
+        # self.num_deep_features = num_deep_features
+
+        #-------wide part:
+        # n: squared sum of past gradients
+        # z: weights
+        # w: lazy weights
+        self.n = np.zeros(D, dtype = np.float32)
+        self.z = np.zeros(D, dtype = np.float32)
+        
+
+        # ---feature related parameters
+        self.D = D
 
 
         # ----Build the embedding layers:  to be passed through the deep-side
@@ -95,19 +110,6 @@ class WideDeep(nn.Module):
         # PART of FC layer for deep side only, the othre half is with "WIDE", implemented outside Pytorch
         self.final_partial_fc = nn.Linear(self.hidden_layers[-1], self.n_class)
 
-
-
-        #=========wide part:
-        # n: squared sum of past gradients
-        # z: weights
-        # w: lazy weights
-        self.n = np.zeros(D, dtype = np.float32)
-        self.z = np.zeros(D, dtype = np.float32)
-        
-        self.interaction = interaction
-
-        # ---feature related parameters
-        self.D = D
 
     def compile(self, optimizer, learning_rate, momentum, alpha, beta, L1, L2,method="logistic"):
         """Wrapper to set the activation, loss and the optimizer.
@@ -143,29 +145,14 @@ class WideDeep(nn.Module):
     def get_transform_spec(self,loader_cols):
         return TransformSpec(func = None, selected_fields = loader_cols)
 
-    def indexing(self,X_w):
-        X_w_indices = np.ones(X_w.shape,dtype = np.int32)
-        for i in range(X_w.shape[0]):
-            for j in range(X_w.shape[1]):
-                X_w_indices[i,j] = abs(hash(self.wide_cols[j]+'_'+ str(X_w[i,j].item()))) % self.D
-        return X_w_indices
-
     def indices_inter(self, x):
         ''' 
-        - x: a list of index of hashed features
-        A helper generator that yields the indices implied by x
-
-        - The purpose of this generator is to make the following
-        code a bit cleaner when doing feature interaction.
-        - this function keeps value for x unchanged, mainly enerate hashing feature index of the interactive features
+        # first yield index of the bias term,note hash(0)=0 in python, liu: but assume we don't have  zero value in the the feature values for wide features?
+        # - yield 0 # bias introduced in deep side
+        # - then yield the normal indices
         '''
+        pass
 
-        # first yield index of the bias term,note hash(0)=0 in python, but assume we don't have  zero value in the the feature values for wide features.
-        # yield 0 # bias introduced in deep side
-
-        # then yield the normal indices
-        for index in x:
-            yield index
 
     def forward_wide(self,x_w_indices,j):
         '''
@@ -212,7 +199,7 @@ class WideDeep(nn.Module):
         # =========Deep Side
         # ---- get embedding concatenated with continuous features: 'deep_inp'
         # -- get embeddings for embedding features: emb (a list of embeddings, one embedding for each feature respectively)
-        emb = [getattr(self, 'emb_layer_'+col)(X_d[:,self.deep_column_idx[col]].long())
+        emb = [getattr(self, 'emb_layer_'+col)(X_d[:,self.deep_column_idx[col]])
                for col,_,_ in self.embeddings_input]
        
         # -- cont is for continuous features
@@ -249,7 +236,7 @@ class WideDeep(nn.Module):
         deep_z = self.final_partial_fc(x_deep)
         # wide
         wide_z = torch.empty(deep_z.shape, requires_grad = False, dtype = deep_z.dtype, device = deep_z.device)
-        # iterate within a batch
+        # iterate over training samples within a batch
         for j in range(X_w_indices.shape[0]):
             wide_z[j] = self.forward_wide(X_w_indices[j,:],j)
 
@@ -270,7 +257,7 @@ class WideDeep(nn.Module):
 
         parameters
         ---------
-        x_wide (iterable):  (num_wide_features,), feature indices on wide side
+        x_wide (iterable):  (num_total_wide_features,), feature indices on wide side
         y_pred (float) : prediction (probablity) from model
         y (float): gound truth
 
@@ -317,13 +304,10 @@ class WideDeep(nn.Module):
 
         with torch.no_grad():
             with converter_test.make_torch_dataloader(batch_size = batch_size, transform_spec = self.get_transform_spec(loader_cols),num_epochs = 1,shuffle_row_groups = False, workers_count = 2) as test_loader:
-                for i, row_dic in enumerate(test_loader):
-                    # decode features
-                    X_batch = row_dic['all_features']
-                    y = row_dic['label']
-                    X_w = X_batch[:,:self.num_wide_features]
-                    X_d = X_batch[:,-self.num_deep_features:]
-                    X_w_indices = self.indexing(X_w) # X_w_indices: np.array
+                for i, row_batch in enumerate(test_loader):
+                    X_w_indices = row_batch['hashed_wide'].int()
+                    X_d = row_batch['embedding_indexed'].long()
+                    y = row_batch['label']
 
                     if use_cuda:
                         X_d, y = X_d.cuda(), y.cuda()
@@ -352,7 +336,7 @@ class WideDeep(nn.Module):
         - n_epochs (int)
         - batch_size (int)
         """
-        self.w_values_array = np.empty((batch_size,self.num_wide_features),dtype = np.float32) # np.array
+        self.w_values_array = np.empty((batch_size,self.num_total_wide_features),dtype = np.float32) # np.array
         train_loss_history = []
         test_loss_history = []
 
@@ -375,13 +359,10 @@ class WideDeep(nn.Module):
 
             # petastorm loader: note rows of data will be expressed as a dictionary, with column names as the keys
             with converter_train.make_torch_dataloader(batch_size = batch_size, transform_spec = self.get_transform_spec(loader_cols),num_epochs = 1,shuffle_row_groups = False, workers_count = 2) as train_loader:
-                for i, row_dic in enumerate(train_loader):
-                    # decode features
-                    X_batch = row_dic['all_features']
-                    y = row_dic['label']
-                    X_w = X_batch[:,:self.num_wide_features]
-                    X_d = X_batch[:,-self.num_deep_features:]
-                    X_w_indices = self.indexing(X_w) # X_w_indices: np.array
+                for i, row_batch in enumerate(train_loader):
+                    X_w_indices = row_batch['hashed_wide'].int()
+                    X_d = row_batch['embedding_indexed'].long()
+                    y = row_batch['label']
 
                     if use_cuda:
                          X_d, y = X_d.cuda(), y.cuda()
@@ -404,7 +385,7 @@ class WideDeep(nn.Module):
                     # running_total+= y.size(0)
                     # running_loss += loss.item() *  y.size(0)
 
-                    #----print out loss for current batch if i is multiple of batch_interval
+                    #----print out loss for current batch if it is multiple of batch_interval
                     running_loss_batch += loss.item() *  y.size(0)
                     running_total_batch += y.size(0)
                     if  i%batch_interval==0 and i!=0:
